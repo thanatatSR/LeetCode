1c79801b45b137fdc8af3b312fe02f9768ee7567
Thanatat Srisung <121552948+thanatatSR@users.noreply.github.com>
2023-05-31 18:48:00 +0700

    Create 13RomanToInteger.js

---
+++ 13RomanToInteger.js
@@ -0,0 +1,33 @@
+const romanToInt = (s) => {
+    if (typeof(s) !== 'string') {
+        return 'Please use input string'
+    } else {
+        const romanValues = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+
+  let sum = 0;
+  let prevValue = 0;
+
+  for (let i = s.length - 1; i >= 0; i--) {
+    const currentValue = romanValues[s[i]];
+    if (currentValue >= prevValue) {
+      sum += currentValue;
+    } else {
+      sum -= currentValue;
+    }
+    prevValue = currentValue;
+  }
+
+  return sum;
+};
+    }
+  
+
+romanToInt("XIII")
